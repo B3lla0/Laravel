108d323f83c18bf4083f0697c522932b853762de
B3lla <sjin09300@gmail.com>
2022-11-30 23:09:20 +0900

    feat : 로그인 기능 추가

--- routes/web.php
+++ routes/web.php
@@ -25,19 +25,21 @@ Route::get('/projects', [ProjectController::class, 'index']);
 Auth::routes();
 
 use App\Http\Controllers\PostlistController;
-Route::get('/postlist', [PostlistController::class, 'index']);
-
-Route::get('/postlist/create', [PostlistController::class, 'create']);
-
-Route::post('/postlist', [PostlistController::class, 'store']);
-
-Route::get('/postlist/{post}', [PostlistController::class, 'show']);
-
-Route::get('/postlist/{post}/edit', [PostlistController::class, 'edit']);
-
-Route::put('/postlist/{post}', [PostlistController::class, 'update']);
-
-Route::delete('/postlist/{post}', [PostlistController::class, 'destroy']);
+Route::prefix('postlist')->middleware('auth')->group(function(){
+  Route::get('/', [PostlistController::class, 'index']);
+  
+  Route::get('/create', [PostlistController::class, 'create']);
+  
+  Route::post('/', [PostlistController::class, 'store']);
+  
+  Route::get('/{post}', [PostlistController::class, 'show']);
+  
+  Route::get('/{post}/edit', [PostlistController::class, 'edit']);
+  
+  Route::put('/{post}', [PostlistController::class, 'update']);
+  
+  Route::delete('/{post}', [PostlistController::class, 'destroy']);
+});
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
